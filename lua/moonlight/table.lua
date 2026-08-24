5712849bf39939ee05c91f560e9b139aaf0e7714
Zaurzo <wafflesfordaysyt@gmail.com>
2026-08-24 13:29:45 -0400

    add table.weak

--- lua/moonlight/table.lua
+++ lua/moonlight/table.lua
@@ -6,6 +6,22 @@ function table.ipack(...)
     return { [0] = select('#', ...), ... }
 end
 
+local weak_metatables = {
+    k = { __mode = 'k' },
+    v = { __mode = 'v' },
+    kv = { __mode = 'kv' }
+}
+
+---@param mode string
+---@return table
+function table.weak(mode)
+    if not mode or not weak_metatables[mode] then
+        return error('invalid mode')
+    end
+
+    return setmetatable({}, weak_metatables[mode])
+end
+
 ---@param tbl table
 ---@return table copy
 function table.ShallowCopy(tbl)
