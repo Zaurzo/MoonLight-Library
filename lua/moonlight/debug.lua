6b635936330b9e599ce92f279c85f7b675f7b25b
Zaurzo <wafflesfordaysyt@gmail.com>
2026-08-24 13:58:11 -0400

    add debug.funcinfo

--- lua/moonlight/debug.lua
+++ lua/moonlight/debug.lua
@@ -17,4 +17,35 @@ function debug.setcacheimports(enabled)
     import._caching_enabled = enabled
 end
 
+local func_info_cache = setmetatable({}, { __mode = 'k' })
+
+---@param func function
+function debug.funcinfo(func)
+    local info = func_info_cache[func]
+    
+    if info then
+        return info
+    end
+
+    moonlight.assert(isfunction(func), 'funcinfo: expected function', 2)
+
+    info = debug.getinfo(func)
+    info.func = nil -- remove strong reference
+
+    ---@diagnostic disable-next-line
+    local jit_info = jit.util.funcinfo(func)
+
+    -- Merge some debug info from jit.util.funcinfo
+    info.loc = jit_info.loc
+    info.gcconsts = jit_info.gcconsts
+    info.nconsts = jit_info.nconsts
+    info.bytecodes = jit_info.bytecodes
+    info.children = jit_info.children
+    info.stackslots = jit_info.stackslots
+
+    func_info_cache[func] = info
+
+    return info
+end
+
 return debug
