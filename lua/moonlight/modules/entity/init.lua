31e59dc3fad8a75f7eb73f221ded60dde6939481
Zaurzo <wafflesfordaysyt@gmail.com>
2026-09-01 00:59:34 -0400

    add entity.Kill method

---
+++ lua/moonlight/modules/entity/init.lua
@@ -0,0 +1,21 @@
+local entity = include('shared.lua')
+
+function entity.Kill(self, dmginfo)
+    entity.SetHealth(self, 0)
+
+    if dmginfo then
+        local dmg = dmginfo:GetDamage()
+
+        if math.IsNearlyEqual(dmg, 0) then
+            dmginfo:SetDamage(1)
+        end
+
+        entity.TakeDamageInfo(self, dmginfo)
+
+        dmginfo:SetDamage(dmg)
+    else
+        entity.TakeDamage(self, 1)
+    end
+end
+
+return entity
