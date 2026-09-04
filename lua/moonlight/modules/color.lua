dc7fe2ea7cc0a5210180d02bacc62ea09cad2abd
Zaurzo <wafflesfordaysyt@gmail.com>
2026-09-03 23:45:04 -0400

    add more colors

--- lua/moonlight/modules/color.lua
+++ lua/moonlight/modules/color.lua
@@ -3,8 +3,13 @@ local color = {
     black = Color(0, 0, 0, 255),
     clear = Color(0, 0, 0, 0),
     red = Color(255, 0, 0),
+    orange = Color(255, 127, 0),
+    yellow = Color(255, 255, 0),
     green = Color(0, 255, 0),
-    blue = Color(0, 0, 255)
+    blue = Color(0, 0, 255),
+    purple = Color(127, 0, 255),
+    pink = Color(255, 0, 255),
+    cyan = Color(0, 255, 255)
 }
 
 function color.Rainbow(speed)
